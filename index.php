897405fb95073033f909839ef30c7c91d998be4c
Sejalkhandelwal-codex <92508542+Sejalkhandelwal-codex@users.noreply.github.com>
2022-10-20 18:10:47 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Find Doctor</a>
+          <a class="nav-link" href="#">Find</a>
         </li>
         <li class="nav-item">
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
@@ -39,7 +39,10 @@
   </div>
 </nav>
  </header>
-<section class="home-banner container d-flex">
+ <div class="main">
+
+
+<!-- <section class="home-banner align-items-center container d-flex">
 <div class="left">
   <h1 class="banner-title">Skip the travel ! <br>
 Take online doctor consultation</h1>
@@ -51,8 +54,129 @@ Take online doctor consultation</h1>
 
 </div>
 
+</section> -->
+<section class="hospital-name p-50">
+  <h2 class="common-title text-center">E-Health Hospitals</h2>
+  <div class="d-flex container card-sec ">
+    <a href="" class="card ">
+  <img src="../images/h-1.svg" alt="img" height="100%" width="100%">
+      <div class="text">
+        
+        <p class="name-h">Hospital Name</p>
+        <span class="location">Location</span>
+      </div>
+    </a>
+    <a href="" class="card">
+    <img src="../images/h-2.svg" alt="img" height="100%" width="100%">
+      <div class="text">
+        
+        <p class="name-h">Hospital Name</p>
+        <span class="location">Location</span>
+      </div>
+    </a>
+    <a href="" class="card">  <img src="../images/h-3.svg" alt="img" height="100%" width="100%">
+      <div class="text">
+        
+        <p class="name-h">Hospital Name</p>
+        <span class="location">Location</span>
+      </div></a>
+    <a href="" class="card">  <img src="../images/h-4.svg" alt="img" height="100%" width="100%">
+      <div class="text">
+        <p class="name-h">Hospital Name</p>
+        <span class="location">Location</span>
+      </div></a>
+  </div>
 </section>
+<section class="service p-50">
+  <div class="container">
+    <div class="row sp"><div class="col-md-6">
 
+    <h2 class="common-title align-items-center">Services</h2>
+    </div>
+  
+    <div class="col-md-6">
+    <p class="common-subtitle">Rumah sakit adalah bagian integral dari suatu organisasi sosial dan kesehatan dengan fungsi menyediakan pelayanan paripurna (komprehensif).</p>
+    
+</div></div>
+
+<div class="service-card d-flex">
+  <div class="card">
+    <figure>
+      
+
+
+
+<svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle opacity="0.1" cx="40" cy="40" r="40" fill="#27AE60"/>
+<path d="M56.483 25.5688C54.4656 23.5293 51.7404 22.4018 48.8092 22.3936C45.7923 22.3835 42.7688 23.5266 40.3464 25.5587C37.5913 23.1325 35.0991 21.9496 32.7523 21.9496C29.4748 21.9496 26.434 23.2404 24.2106 25.5638C22.5395 27.2349 21.3945 29.429 20.8982 31.9092C20.7496 32.6546 21.2326 33.3793 21.978 33.5285C22.7226 33.6762 23.4482 33.1941 23.5973 32.4486C23.9863 30.5033 24.8712 28.7954 26.1776 27.4886C27.8978 25.6913 30.2331 24.7018 32.7524 24.7018C34.5776 24.7018 36.7887 25.9317 39.3006 28.333C39.5823 28.6161 39.9524 28.7725 40.3428 28.7729C40.3433 28.7729 40.3442 28.7729 40.3446 28.7729C40.7341 28.7729 41.1042 28.6175 41.3863 28.3354C43.4143 26.3074 46.1069 25.1459 48.7767 25.1459C48.7855 25.1459 48.7937 25.1459 48.8019 25.1459C51.0267 25.1518 53.0066 25.9679 54.5111 27.4886C56.2762 29.3322 57.2483 31.7468 57.2483 34.289C57.2483 36.8331 56.2542 39.3079 54.5487 41.0518L40.3669 54.761L29.1216 43.8958C28.5752 43.3679 27.7041 43.3839 27.1757 43.9289C26.6477 44.4757 26.6629 45.3468 27.2092 45.8747L39.411 57.6637C39.6775 57.9215 40.0225 58.0504 40.3669 58.0504C40.7114 58.0504 41.0564 57.9215 41.3234 57.6642L56.4885 43.0036C58.7202 40.7225 60 37.5463 60 34.289C60 31.0343 58.7564 27.9431 56.483 25.5688Z" fill="#27AE60"/>
+<path d="M57.523 37.4543H46.422C46.0431 37.4543 45.6816 37.6097 45.422 37.885L41.2491 42.2974L36.4427 31.3946C36.2362 30.9263 35.7867 30.6111 35.2762 30.5768C34.7684 30.5442 34.278 30.7942 34.0105 31.2304L30.1931 37.4543H21.3762C20.616 37.4543 20 38.0703 20 38.8304C20 39.5906 20.616 40.2066 21.3762 40.2066H30.9629C31.4413 40.2066 31.8859 39.958 32.1359 39.5497L34.9836 34.9075L39.5666 45.3029C39.7524 45.725 40.1373 46.0254 40.5914 46.1039C40.6694 46.1171 40.7478 46.124 40.8258 46.124C41.201 46.124 41.5638 45.9708 41.8258 45.6937L47.0148 40.2065H57.523C58.2831 40.2065 58.8992 39.5904 58.8992 38.8303C58.8991 38.0703 58.283 37.4543 57.523 37.4543Z" fill="#27AE60"/>
+</svg>
+
+    </figure>
+    <h3 class="title">Doctors</h3>
+    <p class="text-14">Toko tempat meramu dan menjual obat berdasarkan resep dokter serta memperdagangkan barang medis</p>
+    <a href="" class="common-btn">See All</a>
+  </div>
+  <div class="card">
+  <figure>
+  <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle opacity="0.1" cx="40" cy="40" r="40" fill="#855FCE"/>
+    <path d="M49.2906 20H30.7552C29.9968 20 29.3822 20.6146 29.3822 21.373V28.1006C29.3822 28.859 29.9968 29.4736 30.7552 29.4736H49.2906C50.049 29.4736 50.6636 28.859 50.6636 28.1006V21.373C50.6636 20.6146 50.049 20 49.2906 20ZM47.9177 26.7277H32.1281V22.746H47.9176V26.7277H47.9177Z" fill="#855FCE"/>
+    <path d="M47.3227 28.1467H44.5766V34.4625H47.3227V28.1467Z" fill="#855FCE"/>
+    <path d="M35.4234 28.1467H32.6773V34.4625H35.4234V28.1467Z" fill="#855FCE"/>
+    <path d="M49.9771 33.0892H30.0229C28.7611 33.0892 27.7345 34.1157 27.7345 35.3775V57.7116C27.7345 58.9734 28.7611 60 30.0229 60H49.9771C51.2389 60 52.2655 58.9734 52.2655 57.7116V35.3775C52.2655 34.1157 51.2389 33.0892 49.9771 33.0892ZM49.5195 57.2539H30.4805V35.8352H49.5195V57.2539Z" fill="#855FCE"/>
+    <path d="M40 40.2745C39.2416 40.2745 38.627 40.8891 38.627 41.6475V51.4415C38.627 52.1999 39.2416 52.8145 40 52.8145C40.7583 52.8145 41.373 52.1999 41.373 51.4415V41.6475C41.373 40.8891 40.7583 40.2745 40 40.2745Z" fill="#855FCE"/>
+    <path d="M44.897 45.1717H35.1029C34.3446 45.1717 33.73 45.7863 33.73 46.5446C33.73 47.303 34.3446 47.9176 35.1029 47.9176H44.897C45.6558 47.9176 46.27 47.303 46.27 46.5446C46.27 45.7863 45.6554 45.1717 44.897 45.1717Z" fill="#855FCE"/>
+    </svg>
+  </figure>  
+  <h3 class="title">Medicine</h3>
+    <p class="text-14">Toko tempat meramu dan menjual obat berdasarkan resep dokter serta memperdagangkan barang medis</p>
+  <a href="" class="common-btn">Book Appointment</a></div>
+  <div class="card"> 
+  <figure>
+  <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle opacity="0.1" cx="40" cy="40" r="40" fill="#2F80ED"/>
+<g clip-path="url(#clip0_13_8)">
+<path d="M53.9711 33.2401L46.7594 26.0284C46.268 25.5373 45.473 25.5369 44.9816 26.0275L26.4073 44.56L28.1842 46.3411L45.8693 28.6958L51.304 34.1311L33.6592 51.8157L35.4399 53.5927L53.9724 35.0179C54.4625 34.5264 54.4622 33.7306 53.9711 33.2401Z" fill="#2F80ED"/>
+<path d="M56.1099 25.6515L54.349 23.8905C53.9389 23.4804 53.3024 23.4037 52.8068 23.7047L46.7272 27.3945L48.0324 29.545L53.268 26.3672L53.6328 26.7324L50.4554 31.968L52.606 33.2732L56.2957 27.1935C56.5967 26.6981 56.52 26.0611 56.1099 25.6515Z" fill="#2F80ED"/>
+<path d="M37.7867 54.1633L25.837 42.2136C25.346 41.7222 24.5494 41.7222 24.0584 42.2136C23.567 42.7046 23.567 43.5013 24.0584 43.9922L36.0081 55.9419C36.2534 56.1876 36.5754 56.3104 36.8974 56.3104C37.219 56.3104 37.5415 56.1875 37.7867 55.9419C38.2781 55.4509 38.2781 54.6543 37.7867 54.1633Z" fill="#2F80ED"/>
+<path d="M37.4726 43.1788L25.7336 54.961L27.5155 56.7363L39.2545 44.9541L37.4726 43.1788Z" fill="#2F80ED"/>
+<path d="M35.0011 40.707L23.2621 52.4892L25.0439 54.2645L36.7829 42.4823L35.0011 40.707Z" fill="#2F80ED"/>
+<path d="M30.4491 57.8529L22.1473 49.5506C21.6559 49.0595 20.8592 49.0588 20.3682 49.5506C19.8772 50.0416 19.8772 50.8377 20.3682 51.3292L28.6701 59.6315C28.9158 59.8772 29.2378 60 29.5597 60C29.8813 60 30.2038 59.8771 30.449 59.6315C30.9401 59.1405 30.9401 58.3443 30.4491 57.8529Z" fill="#2F80ED"/>
+<path d="M59.6315 20.3687C59.1401 19.8773 58.3443 19.8773 57.8529 20.3687L53.4503 24.7712C52.9589 25.2622 52.9589 26.0589 53.4503 26.5498C53.696 26.7955 54.0176 26.9183 54.3396 26.9183C54.6617 26.9183 54.9836 26.7954 55.2289 26.5498L59.6314 22.1473C60.1229 21.6564 60.1229 20.8597 59.6315 20.3687Z" fill="#2F80ED"/>
+<path d="M34.5997 38.3131L32.8209 40.092L39.9066 47.1777L41.6854 45.3988L34.5997 38.3131Z" fill="#2F80ED"/>
+</g>
+<defs>
+<clipPath id="clip0_13_8">
+<rect width="40" height="40" fill="white" transform="translate(20 20)"/>
+</clipPath>
+</defs>
+</svg>
+  </figure>  
+  <h3 class="title">Video Consultant</h3>
+    <p class="text-14">Toko tempat meramu dan menjual obat berdasarkan resep dokter serta memperdagangkan barang medis</p>
+  <a href="" class="common-btn">Buy Now</a></div>
+
+</div>
+  </div>
+ 
+ 
+</section>
+<section class="add-hospital p-50 ">
+  <div class="row container align-items-center ">
+<div class="col-md-5">
+  <img src="../images/unsplash.svg" alt="" height="100%" weight="100%">
+</div>
+<div class="col-md-5">
+  <h2 class="common-title">Want to add your hospital with E-health</h2>
+  <p class="common-subtitle">
+  Rumah sakit adalah bagian integral dari suatu organisasi sosial dan kesehatan dengan fungsi menyediakan pelayanan paripurna (komprehensif).
+  </p>
+
+</div>
+  </div>
+</section>
+</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
   
